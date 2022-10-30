f558389cc909d1eba92eedf5642e95ed48479961
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-10-30 17:09:15 -0400

    some resolution

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/components/Home';
+import Home from './src/Screens/Home';
 
 export default function App() {
   return (
