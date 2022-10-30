53287859c3c7c867affd903dff32c6c06f3d86bd
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-10-30 03:09:34 -0400

    Cross platform development project initiated

---
+++ Screens/Home.tsx
@@ -0,0 +1,79 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { useFonts, Abel_400Regular } from '@expo-google-fonts/abel';
+
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+export default function Home() {
+   useFonts({
+        Abel_400Regular,
+      });
+
+        return (
+            <View style={styles.wrapper}>
+            <View style={styles.logo_container}>
+                    <Image style={styles.logo}  
+                        source={{
+                            uri: 'https://reactnative.dev/img/tiny_logo.png',
+                        }}
+                    />
+            </View>
+            <Pressable 
+                onPress={()=>{
+                    console.log("Pressed")
+                }}
+                style={styles.home_btn}
+            >
+                <Text style={styles.home_btn_text}>
+                    Get Started
+                    
+                </Text>
+                <AntDesign name="arrowright" style={
+                    {
+                        fontFamily:'Abel_400Regular',
+                        paddingTop:2,
+                        marginLeft:3
+                    }
+                } size={16} color="white" />
+            </Pressable>
+            
+            </View>
+        );
+}
+const styles = StyleSheet.create({
+    wrapper : {
+        flex:1,
+        justifyContent:'center',
+        alignContent:'center'
+        
+    },
+    logo_container:{
+        justifyContent:'space-around',
+        alignContent:'center'
+    },
+    logo:{
+        width:200,
+        height:200,
+        alignContent:'center'
+    },
+    home_btn:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        marginTop:10,
+        backgroundColor: '#128892',
+        flexDirection: 'row'
+      },
+      home_btn_text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: '#ffffff',
+        fontFamily:'Abel_400Regular',
+      },
+})
