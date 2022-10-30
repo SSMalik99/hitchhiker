618e224f67dccdf777bfc17945aba581ca19ebda
Srijan Shrestha <srijans318@gmail.com>
2022-10-30 16:58:54 -0400

    Changes on login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import { Button, StyleSheet, Text, View, Image, Pressable } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    wrapper: {
+        
+    }
+})
+export default function Login() {
+
+}
