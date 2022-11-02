7a9b2228d1d91cc4e5a7b2d7326169a1f7c21802
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-11-01 22:04:17 -0400

    route name added in header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,75 +1,113 @@
-import { Entypo } from '@expo/vector-icons';
-import { Pressable, View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
-import React, { ReactPropTypes } from 'react';
+import { Entypo } from "@expo/vector-icons";
+import { Pressable, View, Text } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import React, { ReactPropTypes } from "react";
 
+const AppHeader = ({ includeLogin }) => {
+  const navigation = useNavigation();
+  const route = useRoute();
 
-const AppHeader = ({includeLogin}) => {
+  return includeLogin !== true ? (
+    <View style={styles.head}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            navigation.toggleDrawer();
+          }}
+        >
+          <Entypo name="menu" size={44} color="white" />
+        </Pressable>
+        <Text
+          style={{
+            color: "#ffffff",
+            marginLeft: 3,
+            fontSize: 25,
+            fontFamily: "Abel_400Regular",
+          }}
+        >
+          {route.name}
+        </Text>
+      </View>
+    </View>
+  ) : (
+    <View style={styles.head}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            navigation.toggleDrawer();
+          }}
+        >
+          <Entypo name="menu" size={44} color="white" />
+        </Pressable>
 
-    const navigation = useNavigation();
+        <Text
+          style={{
+            color: "#ffffff",
+            marginLeft: 3,
+            fontSize: 25,
+            fontFamily: "Abel_400Regular",
+          }}
+        >
+          {route.name}
+        </Text>
+      </View>
 
-    return includeLogin !== true ?
-        <View style={styles.head}>
-            <Pressable 
-                    onPress={()=>{
-                        navigation.toggleDrawer()
-                    }}
-                    
-                >
-                    <Entypo name="menu" size={24} color="white" />
-                </Pressable>
-        </View>
-    :
-        <View style={styles.head}>
-          
-                <Pressable 
-                        onPress={()=>{
-                            navigation.toggleDrawer()
-                        }}
-                    >
-                    <Entypo name="menu" size={24} color="white" />
-                </Pressable>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems:"center"
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          <Text style={styles.header_link}>Login</Text>
+        </Pressable>
 
-                <View style={
-                    {
-                        flexDirection:"row",
-                        justifyContent:"space-between"
-                    }
-                }>
-                    <Pressable 
-                        onPress={()=>{
-                            navigation.navigate('Login')
-                        }} >
-                        <Text style={styles.header_link}>Login</Text>
-                    </Pressable>
+        <Text
+          style={{
+            marginHorizontal: 8,
+          }}
+        >
+          |
+        </Text>
 
-                    <Text style={{
-                        marginHorizontal:8
-                    }}>|</Text>
-
-                    <Pressable 
-                        onPress={()=>{
-                            navigation.navigate('Signup')
-                        }} >
-                        <Text style={styles.header_link}>Sign Up</Text>
-                    </Pressable>
-                </View>
-          
-        </View>
-    ;
-}
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Signup");
+          }}
+        >
+          <Text style={styles.header_link}>Sign Up</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    head:{
-        backgroundColor:"#128892",
-        flexDirection:"row",
-        justifyContent:'space-between',
-        padding:10,
-        alignContent:'center',
-    },
-    header_link :{
-        color:'#ffffff',
-        fontFamily:"Abel_400Regular"
-    }
-})
-export default AppHeader
+  head: {
+    backgroundColor: "#128892",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    alignContent: "center",
+  },
+  header_link: {
+    color: "#ffffff",
+    fontFamily: "Abel_400Regular",
+  },
+});
+export default AppHeader;
